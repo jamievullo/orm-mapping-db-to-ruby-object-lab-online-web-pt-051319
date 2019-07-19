ddf754d22bbc5cca38a623ab802a330407507c5b
Jamie Vullo <jvullo@protonmail.com>
2019-07-19 14:02:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -92,9 +92,9 @@ class Student
       LIMIT ?
       SQL
       
-      DB[:conn].execute(sql, x)#.map do |row|
-        #self.new_from_db(row)
-      #end
+      DB[:conn].execute(sql, x).map do |row|
+        self.new_from_db(row)
+      end
   end
       
   
