b8f3cbcc523b747c12996eeced12c1b32aa92a3e
Jamie Vullo <jvullo@protonmail.com>
2019-07-19 14:07:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -105,9 +105,9 @@ class Student
       LIMIT 1
       SQL
       
-      DB[:conn].execute(sql).map do |row|
-        self.new_from_db(row)
-      end
+      DB[:conn].execute(sql)#.map do |row|
+        #self.new_from_db(row)
+      #end
   end
       
   
