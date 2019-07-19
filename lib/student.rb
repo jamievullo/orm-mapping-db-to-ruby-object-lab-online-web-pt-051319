74f1d6246055c0131e93aa96674d63a73b055595
Jamie Vullo <jvullo@protonmail.com>
2019-07-19 13:31:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,8 +56,8 @@ class Student
   
   def self.all_students_in_grade_9
     sql <<-SQL
-      SELECT * FROM students 
-      WHERE grade = 9 
+      "SELECT * FROM students 
+      WHERE grade = 9;" 
       SQL
       
       DB[:conn].execute(sql)
