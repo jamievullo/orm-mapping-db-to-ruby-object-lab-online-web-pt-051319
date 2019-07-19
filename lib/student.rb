4229ecd551ef7632a7c88fbb680ed767de1acabe
Jamie Vullo <jvullo@protonmail.com>
2019-07-19 13:39:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,8 +61,16 @@ class Student
       WHERE grade = 9 
       SQL
       
-      DB[:conn].execute(sql)#.map do |row|
-        #self.new_from_db(row)
-      #end.first
+      DB[:conn].execute(sql)
   end 
+  
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12 
+      SQL
+      
+      DB[:conn].execute(sql)
+  end
 end
