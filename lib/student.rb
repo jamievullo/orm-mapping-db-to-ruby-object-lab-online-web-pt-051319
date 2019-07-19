af08b125ed88ecc606c4fec796c34eef4591dd03
Jamie Vullo <jvullo@protonmail.com>
2019-07-19 14:06:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -96,6 +96,19 @@ class Student
         self.new_from_db(row)
       end
   end
+  
+  def swlf.first_student_in_grade_10 
+    sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade = 10 
+      LIMIT 1
+      SQL
+      
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
+  end
       
   
 end
